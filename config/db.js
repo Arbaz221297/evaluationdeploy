6b5bfeb53a9778bf290bdc15df26180567989a4f
your_name_here <arbazshaikh221297@gmail.com>
2023-07-03 22:29:31 +0530

    for deploy

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
 
 // const connection = mongoose.connect("mongodb://127.0.0.1:27017/evaluation")
-const connection = mongoose.connect("mongodb://127.0.0.1:27017/evaluation")
+const connection = mongoose.connect("mongodb+srv://evaluation:Arbaz2212@cluster0.tl2qnlr.mongodb.net/?retryWrites=true&w=majority")
 module.exports = {connection}
